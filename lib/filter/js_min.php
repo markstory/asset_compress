88f58258b05ad0fac4c080d73134643f82d1aa7e
Mark Story <mark@mark-story.com>
2010-09-26 21:55:50 -0400

    Adding note about where to find JsMin.

--- lib/filter/js_min.php
+++ lib/filter/js_min.php
@@ -6,6 +6,7 @@ App::import('Vendor', 'JsMin', array('file' => 'JSMin/JSMin.php'));
  * JsMin filter.
  *
  * Allows you to filter Javascript files through JsMin.  You need to put JsMin in your application's
+ * vendors directories.  You can get it from http://github.com/rgrove/jsmin-php/
  *
  * @package asset_compress
  * @author Mark Story
