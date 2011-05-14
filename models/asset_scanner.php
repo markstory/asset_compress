03d362a15c981e3574aba10385016587639e2c01
Mark Story <mark@mark-story.com>
2011-05-14 10:26:47 -0400

    Starting to move filesystem scanning features into a class that can be easily reused inside a filter class.

--- models/asset_scanner.php
+++ models/asset_scanner.php
@@ -13,8 +13,63 @@ class AssetScanner {
  */
 	protected $_paths = array();
 
-	public function __construct($paths) {
+	public function __construct(array $paths) {
 		$this->_paths = $paths;
+		$this->_normalizePaths();
+		$this->_expandPaths();
+	}
+
+/**
+ * Ensure all paths end in a DS.
+ *
+ * @return void
+ */
+	protected function _normalizePaths() {
+		foreach ($this->_paths as &$path) {
+			$path = rtrim($path, DS) . DS;
+			$path = $this->_replacePathConstants($path);
+		}
+	}
+
+/**
+ * Replaces the file path constants used in Config files.
+ * Will replace APP and WEBROOT
+ *
+ * @param string $path Path to replace constants on
+ * @return string constants replaced
+ */
+	protected function _replacePathConstants($path) {
+		$constantMap = array('APP/' => APP, 'WEBROOT/' => WWW_ROOT);
+		return str_replace(array_keys($constantMap), array_values($constantMap), $path);
+	}
+
+/**
+ * Takes any configured path that ends in * and expands that to be all 
+ * directories within it.
+ *
+ * @return void
+ */
+	protected function _expandPaths() {
+		$expanded = array();
+		foreach ($this->_paths as $path) {
+			if (substr($path, -1) == '*') {
+				$tree = $this->_generateTree($path);
+				$expanded = array_merge($expanded, $tree);
+			} else {
+				$expanded[] = $path;
+			}
+		}
+		$this->_paths = $expanded;
+	}
+
+/**
+ * Discover all the sub directories for a given path.
+ *
+ * @param string $path The path to search
+ * @return array Array of subdirectories.
+ */
+	protected function _generateTree($path) {
+		return array();
 	}
 
 /**
@@ -24,6 +79,21 @@ class AssetScanner {
  * @return mixed Either false on a miss, or the contents of the file.
  */
 	public function find($file) {
-	
+		foreach ($this->_paths as $path) {
+			if (file_exists($path . $file)) {
+				return $path . $file;
+			}
+		}
+		return false;
 	}
+
+/**
+ * Accessor for paths.
+ *
+ * @return array an array of paths.
+ */
+	public function paths() {
+		return $this->_paths;
+	}
+
 }
