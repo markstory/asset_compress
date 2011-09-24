3ce21fca2f978660d68b2ca6e5944ed0dce32555
evilbloodydemon <evilbloodydemon@gmail.com>
2011-09-24 20:27:52 +0400

    making SprocketsTest run

--- Test/Case/Lib/Filter/SprocketsTest.php
+++ Test/Case/Lib/Filter/SprocketsTest.php
@@ -1,11 +1,11 @@
 <?php
-App::import('Lib', 'AssetCompress.filter/Sprockets');
+App::uses('Sprockets', 'AssetCompress.Filter');
 
 class SprocketsTest extends CakeTestCase {
 
 	function setUp() {
 		$this->_pluginPath = App::pluginPath('AssetCompress');
-		$this->_jsDir = $this->_pluginPath . 'tests' . DS . 'test_files' . DS . 'js' . DS;
+		$this->_jsDir = $this->_pluginPath . 'Test' . DS . 'test_files' . DS . 'js' . DS;
 
 		$this->filter = new Sprockets();
 		$settings = array(
