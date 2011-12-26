c566ae59d065d38814342face378ca8c056c42e1
mark_story <mark@mark-story.com>
2011-12-25 22:58:50 -0500

    Adding background image timestampping.

---
+++ Lib/Filter/TimestampImage.php
@@ -0,0 +1,79 @@
+<?php
+App::uses('AssetFilter', 'AssetCompress.Lib');
+
+/**
+ * Adds timestamp querystrings to all background images in CSS files.
+ * This helps with cachebusting CSS sprites.  This is useful in 
+ * development, and deployment to ensure you always have the most recent 
+ * images.
+ *
+ * @package asset_compress
+ */
+class TimestampImage extends AssetFilter {
+
+/**
+ * Regex for `background` CSS property.
+ *
+ * @var string
+ */
+	protected $_backgroundPattern = '/^(?<prop>.*background\s*\:\s*(?:\#[a-f0-9A-F]{3,6})?\s*url\([\'"]?)(?<path>[^\'")]+?(?:png|gif|jpg))(?<trail>[\'"]?\))/m';
+
+/**
+ * Regex for `background-image` CSS property.
+ *
+ * @var string
+ */
+	protected $_backgroundImagePattern = '/^(?<prop>.*background-image\s*\:\s*url\([\'"]?)(?<path>[^\'")]+?(?:png|gif|jpg))(?<trail>[\'"]?\))/m';
+
+	protected $filename;
+
+/**
+ * Input filter.  Locates CSS background images relative to the 
+ * filename and gets the filemtime for the images.
+ *
+ * @param string $filename The file being processed
+ * @param string $content The file content
+ * @return The content with images timestampped.
+ */
+	public function input($filename, $content) {
+		$this->_filename = $filename;
+		$content = preg_replace_callback($this->_backgroundPattern, array($this, '_replace'), $content);
+		$content = preg_replace_callback($this->_backgroundImagePattern, array($this, '_replace'), $content);
+		return $content;
+	}
+
+/**
+ * Do replacements.
+ *
+ * $matches[0] -> whole background line.
+ * $matches[path] -> the url with any wrapping '/"
+ *
+ * @param array $matches Array of matches
+ * @return string Replaced code.
+ */
+	protected function _replace($matches) {
+		$path = $matches['path'];
+		$imagePath = realpath(dirname($this->_filename) . DS . $path);
+		if (file_exists($imagePath)) {
+			$path = $this->_timestamp($imagePath, $path);
+		}
+		return $matches['prop'] . $path . $matches['trail'];
+	}
+
+/**
+ * Add timestamps to the given path.  Will not change paths with 
+ * querystrings, as they could have anything in them or be customized 
+ * already.
+ *
+ * @param string $filepath The absolute path to the file for timestampping
+ * @param string $path The path to append a timestamp to.
+ * @return string Path with a timestamp.
+ */
+	protected function _timestamp($filepath, $path) {
+		if (strpos($path, '?') === false) {
+			$path .= '?t=' . filemtime($filepath);
+		}
+		return $path;
+	}
+
+}
