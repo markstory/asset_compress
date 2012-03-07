c91ddc6e29b21ab3b7311fdbcd272dc5ee016a62
nojimage <nojimage@gmail.com>
2012-03-07 20:19:31 +0900

    Add LessPHP filter

---
+++ Lib/Filter/LessPHP.php
@@ -0,0 +1,37 @@
+<?php
+
+App::uses('AssetFilter', 'AssetCompress.Lib');
+
+/**
+ * Pre-processing filter that adds support for LESS.css files.
+ *
+ * Requires lessphp to be installed.
+ *
+ * eg. git submodule add https://github.com/leafo/lessphp.git app/Vendor/lessphp
+ *
+ * @see http://leafo.net/lessphp
+ */
+class LessPHP extends AssetFilter {
+
+	protected $_settings = array(
+		'ext' => '.less',
+		'path' => 'lessphp/lessc.inc.php',
+	);
+
+	/**
+	 * Runs `lessc` against any files that match the configured extension.
+	 *
+	 * @param string $filename The name of the input file.
+	 * @param string $input The content of the file.
+	 * @return string
+	 */
+	public function input($filename, $input) {
+		if (substr($filename, strlen($this->_settings['ext']) * -1) !== $this->_settings['ext']) {
+			return $input;
+		}
+		App::import('Vendor', 'lessc', array('file' => $this->_settings['path']));
+		$lc = new lessc($filename);
+		return $lc->parse();
+	}
+
+}
