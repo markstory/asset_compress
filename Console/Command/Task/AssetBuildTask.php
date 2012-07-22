dcbda54af764f217de53c85e9759c34d854453cd
euromark <euromark@web.de>
2012-07-22 17:16:59 +0200

    correct app uses

--- Console/Command/Task/AssetBuildTask.php
+++ Console/Command/Task/AssetBuildTask.php
@@ -1,5 +1,5 @@
 <?php
-App::uses('Shell', 'Console/Command');
+App::uses('Shell', 'Console');
 App::uses('AssetConfig', 'AssetCompress.Lib');
 App::uses('AssetCompiler', 'AssetCompress.Lib');
 App::uses('AssetCache', 'AssetCompress.Lib');
