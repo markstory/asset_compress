markstory/asset_compress

commit be64ecff404b0ecebfd7866806793dba4205bbbe

fix to make sprocket work on windows, too

Lib/Filter/Sprockets.php
@@ -17,7 +17,7 @@ class Sprockets extends AssetFilter {
  *
  * @var stgin
  */
-	protected $_pattern = '/^\s?\/\/\=\s+require\s+([\"\<])([^\"\>]+)[\"\>]\n+/m';
+	protected $_pattern = '/^\s?\/\/\=\s+require\s+([\"\<])([^\"\>]+)[\"\>][\r]\n+/m';
 
 /**
  * A list of unique files already processed.

Test/Case/Lib/Filter/ScssFilterTest.php
@@ -11,13 +11,13 @@ class ScssFilterTest extends CakeTestCase {
 	}
 
 	function testParsing() {
-		$content = file_get_contents($this->_cssDir . DS . 'test.scss');
+		$content = file_get_contents($this->_cssDir . 'test.scss');
 
-		$result = $this->filter->input($this->_cssDir . DS . 'test.scss', $content);
+		$result = $this->filter->input($this->_cssDir . 'test.scss', $content);
 		
-		$expected = file_get_contents($this->_cssDir . DS . 'compiled_scss.css');
+		$expected = file_get_contents($this->_cssDir . 'compiled_scss.css');
 		
-		$this->assertEqual($result, $expected);
+		$this->assertEquals($expected, $result);
 	 }
 
 }

Test/Case/Lib/Filter/SprocketsTest.php
@@ -30,7 +30,7 @@ var Template = new Class({
 });
 
 TEXT;
-		$this->assertEqual($result, $expected);
+		$this->assertTextEquals($expected, $result);
 	 }
 
 	function testInputWithRecursion() {
@@ -49,7 +49,7 @@ var NestedClass = BaseClassTwo.extend({
 
 });
 TEXT;
-		$this->assertEqual($result, $expected);
+		$this->assertTextEquals($expected, $result);
 	}
 
 	function testDoubleInclusion() {
@@ -66,7 +66,7 @@ var DoubleInclusion = new Class({
 
 });
 TEXT;
-		$this->assertEqual($result, $expected);
+		$this->assertTextEquals($expected, $result);
 	}
 
 /**
@@ -96,7 +96,7 @@ var Slideshow = new Class({
 
 });
 TEXT;
-		$this->assertEqual($result, $expected);
+		$this->assertTextEquals($expected, $result);
 	}
 
 /**
@@ -124,6 +124,6 @@ var DoubleInclusion = new Class({
 
 });
 TEXT;
-		$this->assertEqual($result, $expected);
+		$this->assertTextEquals($expected, $result);
 	}
 }

commit f2ef2353ec2bd6ac55c1f241623fe68c71044a06

Fix broken regex.
The sprockets regex was broken in [be64ecff404b0ecebfd7866806793dba4205bbbe]
this should fix it.

Lib/Filter/Sprockets.php
@@ -17,7 +17,7 @@ class Sprockets extends AssetFilter {
  *
  * @var stgin
  */
-	protected $_pattern = '/^\s?\/\/\=\s+require\s+([\"\<])([^\"\>]+)[\"\>][\r]\n+/m';
+	protected $_pattern = '/^\s?\/\/\=\s+require\s+([\"\<])([^\"\>]+)[\"\>](?:[\r\n]+|[\n]+)/m';
 
 /**
  * A list of unique files already processed.