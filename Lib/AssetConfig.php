7cf478170d42756b1467f8b2e554a2c1727a3de8
Cees-Jan <Cees-Jan@xps8500.home.wyrihaxim.us>
2013-04-18 21:35:08 +0200

    Aside from the base asset_compress.ini all loaded plugins are scanned for asset_compress.ini files. When found they are applied  to the config instance

--- Lib/AssetConfig.php
+++ Lib/AssetConfig.php
@@ -99,10 +99,8 @@ class AssetConfig {
 		if ($parsedConfig = Cache::read(self::CACHE_ASSET_CONFIG_KEY, self::CACHE_CONFIG)) {
 			return $parsedConfig;
 		}
-		$contents = self::_readConfig($iniFile);
-
-		$modifiedTime = filemtime($iniFile);
-		return self::_parseConfig($contents, $constants, $modifiedTime);
+		
+		return self::_parseConfig($iniFile, $constants);
 	}
 
 /**
@@ -163,8 +161,34 @@ class AssetConfig {
  * @param int $modifiedTime The modified time of the config data.
  * @return AssetConfig
  */
-	protected static function _parseConfig($config, $constants, $modifiedTime) {
-		$AssetConfig = new AssetConfig(self::$_defaults, $constants, $modifiedTime);
+	protected static function _parseConfig($baseFile, $constants, $modifiedTime = null) {
+            $AssetConfig = new AssetConfig(self::$_defaults, $constants, $modifiedTime);
+            self::_parseConfigFile($baseFile, $AssetConfig);
+            
+            $plugins = CakePlugin::loaded();
+            foreach ($plugins as $plugin) {
+                if (file_exists(CakePlugin::path($plugin) . 'Config' . DS . 'asset_compress.ini')) {
+                    self::_parseConfigFile(CakePlugin::path($plugin) . 'Config' . DS . 'asset_compress.ini', $AssetConfig, $plugin . '.');
+                }
+            }
+            
+            if ($AssetConfig->general('cacheConfig')) {
+                    Cache::write(self::CACHE_ASSET_CONFIG_KEY, $AssetConfig, self::CACHE_CONFIG);
+            }
+            
+            return $AssetConfig;
+        }
+        
+/**
+ * Reads a config file and applies it to the given config instance
+ *
+ * @param string $iniFile Contents to apply to the config instance.
+ * @param AssetConfig $AssetConfig The config instance instance we're applying this config file to.
+ * @param string $prefix Prefix for the target key
+ */
+	protected static function _parseConfigFile($iniFile, $AssetConfig, $prefix = '') {
+                $config = self::_readConfig($iniFile);
+                
 		foreach ($config as $section => $values) {
 			if (in_array($section, self::$_extensionTypes)) {
 				// extension section, merge in the defaults.
@@ -193,14 +217,9 @@ class AssetConfig {
 				}
 
 				// must be a build target.
-				$AssetConfig->addTarget($key, $values);
+				$AssetConfig->addTarget($prefix . $key, $values);
 			}
 		}
-
-		if ($AssetConfig->general('cacheConfig')) {
-			Cache::write(self::CACHE_ASSET_CONFIG_KEY, $AssetConfig, self::CACHE_CONFIG);
-		}
-		return $AssetConfig;
 	}
 
 /**
