22d9ac7337f720311c37b1a7edd1118f6f477fc5
QTSdev <contact@qtsdev.com>
2014-03-31 22:55:00 -0300

    Added SimpleCssMin filter

---
+++ Lib/Filter/SimpleCssMin.php
@@ -0,0 +1,32 @@
+<?php
+App::uses('AssetFilter', 'AssetCompress.Lib');
+
+/**
+ * SimpleCssMin filter.
+ * Easily compresses CSS files. Allows to circuveim problems that some minifiers have
+ * with previously processed files.
+ */
+class SimpleCssMin extends AssetFilter {
+
+/**
+ * Apply SimpleCssMin to $content.
+ * Code credits to Manas Tungare, available in https://gist.github.com/manastungare/2625128.
+ *
+ * @param string $filename target filename
+ * @param string $content Content to filter.
+ * @return string
+ */
+	public function output($filename, $content) {
+		// Remove comments
+		$content = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $content);
+		 
+		// Remove space after colons
+		$content = str_replace(': ', ':', $content);
+		 
+		// Remove whitespace
+		$content = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $content);
+         
+		return $content;
+	}
+
+}
