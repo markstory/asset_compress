6879fafc4f22ef5fae9cd2132ee084fcdf576319
QTSdev <contact@qtsdev.com>
2014-04-01 10:40:22 -0300

    Improved minification. Added test case.

--- Lib/Filter/SimpleCssMin.php
+++ Lib/Filter/SimpleCssMin.php
@@ -10,7 +10,7 @@ class SimpleCssMin extends AssetFilter {
 
 /**
  * Apply SimpleCssMin to $content.
- * Code credits to Manas Tungare, available in https://gist.github.com/manastungare/2625128.
+ * Based on code by Manas Tungare (https://gist.github.com/manastungare/2625128).
  *
  * @param string $filename target filename
  * @param string $content Content to filter.
@@ -19,12 +19,28 @@ class SimpleCssMin extends AssetFilter {
 	public function output($filename, $content) {
 		// Remove comments
 		$content = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $content);
+		
+		// Replace newlines with spaces
+		// (replacing by empty string may break media queries conditions that are splitted over multiple lines)
+		$content = preg_replace('/\n/m', ' ', $content);
+		
+		// Replace consecutive whitespaces by single one
+		$content = preg_replace('/\s{2,}/', ' ', $content);
 		 
-		// Remove space after colons
-		$content = str_replace(': ', ':', $content);
-		 
-		// Remove whitespace
-		$content = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $content);
+		// Remove spaces before and after any of { } , : >
+		$content = preg_replace('/\s*({|}|,|\:|;|>)\s*/', '$1', $content);
+		
+		// Remove spaces left parenthesis or before right parenthesis
+		$content = preg_replace('/(\()\s*|\s*(\))/', '$1$2', $content);
+		
+		// Replace ;} with }
+		$content = preg_replace('/;}/', '}', $content);
+		
+		// Hex colors compression
+		$content = preg_replace('/#(.)\1(.)\2(.)\3/', '#$1$2$3', $content);
+		
+		// Trim
+		$content = preg_replace('/^\s*|\s*$/', '', $content);
          
 		return $content;
 	}
