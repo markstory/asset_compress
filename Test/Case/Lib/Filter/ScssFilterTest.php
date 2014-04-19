bb930276633fa355857cf165c59c08bb02d13e60
Mark Story <mark@mark-story.com>
2014-04-18 22:08:23 -0400

    Fix ScssFilter failing when sass is not installed in /usr/bin/sass.

--- Test/Case/Lib/Filter/ScssFilterTest.php
+++ Test/Case/Lib/Filter/ScssFilterTest.php
@@ -14,6 +14,7 @@ class ScssFilterTest extends CakeTestCase {
 		$this->skipIf(DS === '\\', 'Requires ruby and sass rubygem to be installed');
 		$hasSass = `which sass`;
 		$this->skipIf(empty($hasSass), 'Requires ruby and sass to be installed');
+		$this->filter->settings(array('sass' => trim($hasSass)));
 
 		$content = file_get_contents($this->_cssDir . 'test.scss');
 		$result = $this->filter->input($this->_cssDir . 'test.scss', $content);
