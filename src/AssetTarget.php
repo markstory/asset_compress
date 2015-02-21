de70bbc7ce3aa7edbb188a974319212b1a08b8a3
Mark Story <mark@mark-story.com>
2015-02-20 21:50:07 -0500

    Add paths() to AssetTarget.
    Because assets can have filters that need access to the look up paths,
    they should be added to the target. This allows the Target to contain
    all the information needed to build the target, and solves dependency
    issues for filters like Sprockets and ImportInline.

--- src/AssetTarget.php
+++ src/AssetTarget.php
@@ -10,27 +10,36 @@ namespace AssetCompress;
 class AssetTarget
 {
     protected $path;
-    protected $files;
-    protected $filters;
+    protected $files = [];
+    protected $filters = [];
+    protected $paths = [];
     protected $themed;
 
     /**
      * @param string $path The output path or the asset target.
      * @param array $files An array of AssetCompress\File\FileInterface
      * @param array $filters An array of filter names for this target.
+     * @param array $paths A list of search paths for this asset. These paths
+     *   are used by filters that allow additional resources to be included e.g. Sprockets
      * @param bool $themed Whether or not this file should be themed.
      */
-    public function __construct($path, $files = [], $filters = [], $themed = false)
+    public function __construct($path, $files = [], $filters = [], $paths = [], $themed = false)
     {
         $this->path = $path;
         $this->files = $files;
         $this->filters = $filters;
+        $this->paths = $paths;
         $this->themed = $themed;
     }
 
     public function isThemed()
     {
         return $this->themed;
+    }
+
+    public function paths()
+    {
+        return $this->paths;
     }
 
     public function files()
