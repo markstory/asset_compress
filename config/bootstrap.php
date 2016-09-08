1f0d2c0f1394bef16ca621a2ed6035cfd5620814
Mark Story <mark@mark-story.com>
2016-09-07 23:15:12 -0400

    Bind middleware if the Application class exists.
    Use the presence of the App\Application class to infer that PSR7
    middleware is in use. Otherwise fallback to the dispatch filter.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,5 +1,18 @@
 <?php
 
+use AssetCompress\Middleware\AssetCompressMiddleware;
 use Cake\Routing\DispatcherFactory;
+use Cake\Core\Configure;
+use Cake\Event\EventManager;
 
-DispatcherFactory::add('AssetCompress.AssetCompressor');
+$appClass = Configure::read('App.namespace') . '\Application';
+if (class_exists($appClass)) {
+    // Bind the middleware class after the error handler, or at the end
+    // of the queue. We want to be after the error handler so 404's render nicely.
+    EventManager::instance()->on('Server.buildMiddleware', function ($queue) {
+        $middleware = new AssetCompressMiddleware();
+        $queue->insertAfter('Cake\Error\Middleware\ErrorHandlerMiddleware', $middleware);
+    });
+} else {
+    DispatcherFactory::add('AssetCompress.AssetCompressor');
+}
