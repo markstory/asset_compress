f12075868b9a6d41facf287c14f2e644cdb39b71
Mark Story <mark@mark-story.com>
2016-09-08 22:06:15 -0400

    More PHPCS.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
 use AssetCompress\Middleware\AssetCompressMiddleware;
-use Cake\Routing\DispatcherFactory;
 use Cake\Core\Configure;
 use Cake\Event\EventManager;
+use Cake\Routing\DispatcherFactory;
 
 $appClass = Configure::read('App.namespace') . '\Application';
 if (class_exists($appClass)) {
