86db8cde7418961efd470c6de3084b83a8e80f3e
Yevgeny Tomenko <skie@mail.ru>
2019-05-30 18:23:13 +0300

    cleanup obsolete code

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,10 +1,1 @@
 <?php
-use AssetCompress\Middleware\AssetCompressMiddleware;
-use Cake\Core\Configure;
-use Cake\Event\EventManager;
-use Cake\Routing\DispatcherFactory;
-
-$appClass = Configure::read('App.namespace') . '\Application';
-// if (!class_exists($appClass)) {
-    // deprecationWarning('You need to upgrade to the new Http Libraries to use AssetCompress.');
-// }
